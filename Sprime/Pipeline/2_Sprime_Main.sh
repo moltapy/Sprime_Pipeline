58ef0a428ea8df5648adb3b3f527a98d993790e7
moltapy <2263423153@qq.com>
2024-02-28 11:42:50 +0800

    Update 2_Sprime_Main.sh

--- Sprime/Pipeline/2_Sprime_Main.sh
+++ Sprime/Pipeline/2_Sprime_Main.sh
@@ -1,6 +1,24 @@
+##! /bin/bash
+#source 0_Extract_Sample_List.sh
+#source Merge_plink_Map.sh
+## run sprime
+#outgroup=$outgroup_file
+#gt=$all_autos
+#
+#for chr in {1..22}
+#do
+#map=$map_merge
+##prefix
+#out=chb.yri.${chr}
+#
+#eval "time java -jar sprime.jar gt=${gt} outgroup=${outgroup} map=${map} out=${out} chrom=${chr}" 
+#done
+
 #! /bin/bash
-source 0_Extract_Sample_List.sh
-source Merge_plink_Map.sh
+outgroup_file=../output/outgroup.txt
+all_autos=../output/final/all.auto.vcf.gz
+map_merge=../Data/GRCh37map/plink.all.GRch37.map
+mkdir ../output/sprime
 # run sprime
 outgroup=$outgroup_file
 gt=$all_autos
@@ -9,7 +27,7 @@ for chr in {1..22}
 do
 map=$map_merge
 #prefix
-out=chb.yri.${chr}
+out=../output/sprime/chb.yri.${chr}
 
 eval "time java -jar sprime.jar gt=${gt} outgroup=${outgroup} map=${map} out=${out} chrom=${chr}" 
 done
