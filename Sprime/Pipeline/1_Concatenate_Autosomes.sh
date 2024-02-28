f536550ba670e4211b38776741edd5add7051395
moltapy <2263423153@qq.com>
2024-02-28 11:38:22 +0800

    Update 1_Concatenate_Autosomes.sh

--- Sprime/Pipeline/1_Concatenate_Autosomes.sh
+++ Sprime/Pipeline/1_Concatenate_Autosomes.sh
@@ -1,7 +1,17 @@
+##! /bin/bash
+##不能用source，不然会重跑一遍
+##source 0_Extract_Sample_List.sh
+#mkdir ../output/final
+#all_autos=../output/final/all.auto.vcf.gz
+## 根据vcf.file.list合并vcf_file成为一个，并通过eval(类似python的execuable，执行字符串中的指令)
+#eval "time bcftools concat --file-list $vcf_file_list --naive --output-type z --output $all_autos" 
+
 #! /bin/bash
-source 0_Extract_Sample_List.sh
+vcf_file_list=../output/vcf.file.list
 mkdir ../output/final
 all_autos=../output/final/all.auto.vcf.gz
 # 根据vcf.file.list合并vcf_file成为一个，并通过eval(类似python的execuable，执行字符串中的指令)
-eval "time bcftools concat --file-list $vcf_file_list --naive --output-type z --output $all_autos" 
+#contig不同所以需要用-force
+eval "time bcftools concat --file-list $vcf_file_list --naive-force --output-type z --output $all_autos" 
+
 
