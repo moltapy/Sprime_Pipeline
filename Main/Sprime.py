52c8193219a93d8233e056a8a12e5dfc9e8cca4e
moltapy <2263423153@qq.com>
2024-06-24 19:24:30 +0800

    Update Sprime.py

--- Main/Sprime.py
+++ Main/Sprime.py
@@ -5,4 +5,10 @@ try:
 except:
     subprocess.run("pip install pyyaml -i https://pypi.tuna.tsinghua.edu.cn/simple")
 import yaml
-
+config_path = input("Input path of config file here, default to config:\n")
+if not config_path:
+    config_path = <path_to_config.yaml>
+with open(config_path,"rt") as inyaml:
+    config = yaml.load(inyaml.read(),Loader=yaml.FullLoader)
+    print(f"""Please Ensure the Following Paths:
+          """)
