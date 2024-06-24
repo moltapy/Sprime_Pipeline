4ccc0d308e315c60b745abda5184f1bed0effa00
moltapy <2263423153@qq.com>
2024-06-24 23:52:02 +0800

    renew some scripts

--- Main/Sprime.py
+++ Main/Sprime.py
@@ -7,8 +7,12 @@ except:
 import yaml
 config_path = input("Input path of config file here, default to config:\n")
 if not config_path:
-    config_path = <path_to_config.yaml>
+    config_path = "./config.yaml"
 with open(config_path,"rt") as inyaml:
     config = yaml.load(inyaml.read(),Loader=yaml.FullLoader)
     print(f"""Please Ensure the Following Paths:
           """)
+
+    #······前面列举好这些名字
+    # conda 中bcftools环境的名字也要一并汇入yaml中
+    command_01 = []
